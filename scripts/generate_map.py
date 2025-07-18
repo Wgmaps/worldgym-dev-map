d94c0e2817f9b665f0643ab33886ed0fa67fcb6b
Wgmaps <phoebeya0415@hotmail.com.tw>
2025-07-18 12:52:04 +0800

    Add files via upload

--- scripts/generate_map.py
+++ scripts/generate_map.py
@@ -46,7 +46,7 @@ def generate_map_for_folder(gpx_folder):
                         note = properties.get("note", "")
                         emoji = properties.get("emoji", "")
 
-                        popup_html = f"<div style='font-size:14px; line-height:1.4;'>📍 {name}<br>{note}</div>"
+                        popup_html = f"""<div style='font-size:14px; line-height:1.4; white-space:nowrap;'>📍 {name}<br>{note}</div>"""
 
                         folium.Marker(
                             location=[lat, lon],
